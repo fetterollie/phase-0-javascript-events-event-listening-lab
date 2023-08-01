2934263cf6c19d59bc292a0660fab6cc43a08d9f
Jonathan <jonathan.fetterolf@gmail.com>
2023-08-01 11:54:28 -0400

    adding function for ease of changing alert

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const input = document.getElementById('button');
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+// input.addEventListener('click', clickAlert);
+
 function addingEventListener() {
     const input = document.getElementById('button');
-    input.addEventListener('click', function() {
-        alert('I was clicked!');
-    });
-}
+    input.addEventListener('click', clickAlert);
+};
+
+addingEventListener();
